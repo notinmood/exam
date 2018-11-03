019bf7fba16210e2417ec4c2ab442fdd2747d08c
xiedali <9727005@qq.com>
2018-11-03 09:02:41 +0800

    添加共小程序使用的api

---
+++ code/Application/Game/Controller/ApiController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiedali
+ * Date: 2018/11/3
+ * Time: 8:58
+ */
+
+namespace Game\Controller;
+
+
+use Think\Controller;
+
+class ApiController extends Controller
+{
+    public function index(){
+        echo "hello world!";
+    }
+}
