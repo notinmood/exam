1aa66a0761336350bb5350832e80ef2bd7d43e89
xiedali <9727005@qq.com>
2018-11-05 19:41:37 +0800

    555

---
+++ code/ThinkPHP/Library/Vendor/Hiland/Biz/Tencent/Common/MiniProgramConfig.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiedali
+ * Date: 2018/11/5
+ * Time: 19:33
+ */
+
+namespace Vendor\Hiland\Biz\Tencent\Common;
+
+
+class MiniProgramConfig
+{
+    const APPID= "wxa37839e8d0954603";
+    const SECRET= "96acf4487c365efb37edd16f5bf1b496";
+}
---
+++ code/ThinkPHP/Library/Vendor/Hiland/Biz/Tencent/MiniProgramHelper.class.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiedali
+ * Date: 2018/11/5
+ * Time: 19:31
+ */
+
+namespace Vendor\Hiland\Biz\Tencent;
+
+
+use Vendor\Hiland\Biz\Tencent\Common\MiniProgramConfig;
+
+class MiniProgramHelper
+{
+    /** 根据微信小程序wx.login中success中返回的code获取 用户session信息
+     * @param $code
+     * @return json
+     */
+    private static function getUserSession($code){
+        $APPID= MiniProgramConfig::APPID;
+        $SECRET= MiniProgramConfig::SECRET;
+        $url= "https://api.weixin.qq.com/sns/jscode2session?appid=$APPID&secret=$SECRET&js_code=$code&grant_type=authorization_code";
+        $result= NetHelper::request($url);
+        return $result;
+    }
+    public static function getOpenID($code){
+        $session= self::getUserSession($code);
+        dump($session);
+        $ss= json_decode($session);
+        dump($ss);
+    }
+}
